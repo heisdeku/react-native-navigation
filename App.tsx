5e2d39cb2a30596429d3afddff1fc16665b5673b
Deku || Feranmi <adedokunoluwaferanmi@gmail.com>
2022-06-14 19:17:03 +0100

    feat: bottom tab navigator

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import DrawerNavigation from "./navigation/DrawerNavigation";
 import { View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+
+//navigation components
+import DrawerNavigation from "./navigation/DrawerNavigation";
+import TabNavigation from "./navigation/BottomTabNavigation";
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <DrawerNavigation />
+      <TabNavigation />
     </View>
   );
 }
---
+++ navigation/BottomTabNavigation.tsx
@@ -0,0 +1,119 @@
+import * as React from "react";
+import { Text, View, Button, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
+function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#03cafc",
+      }}
+    >
+      <Text style={{ fontSize: 20, color: "#fff", fontWeight: "800" }}>
+        Home is here
+      </Text>
+    </View>
+  );
+}
+
+function Contact({ navigation }: { navigation: any }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#c203fc",
+      }}
+    >
+      <Text style={{ fontSize: 20, color: "#fff", fontWeight: "800" }}>
+        Contact
+      </Text>
+      <Button title="go back" onPress={() => navigation.goBack} />
+    </View>
+  );
+}
+
+function About({ navigation }: { navigation: any }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#48d696",
+      }}
+    >
+      <Text style={{ fontSize: 20, color: "#fff", fontWeight: "800" }}>
+        About
+      </Text>
+      <Button title="go back" onPress={() => navigation.goBack} />
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{ activeTintColor: "#c91e63" }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+              name={Platform.OS === "ios" ? "ios-home" : "md-home"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Contact"
+        component={Contact}
+        options={{
+          tabBarLabel: "Contact",
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+              name={Platform.OS === "ios" ? "ios-contacts" : "md-mail-open"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="About"
+        component={About}
+        options={{
+          tabBarLabel: "About",
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+              name={Platform.OS === "ios" ? "ios-information-circle" : "md-information-circle"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function TabNavigation() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
